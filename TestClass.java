efeb1d70c049e6516f6db567362d36b9a10f2f86
hildakim <pinkredjohn@gmail.com>
2020-01-14 16:10:02 +0900

    local change

--- TestClass.java
+++ TestClass.java
@@ -4,6 +4,6 @@ public class TestClass{
     
     public static void main(String[] args){
         System.out.println("Hi!!Hello!!");
-        System.out.println("I'm hungry!!");
+        System.out.println("I'm hungry~~~!!");
     }
 }
